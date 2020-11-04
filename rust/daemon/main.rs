df6d595731c9572f2588e72e74beb2af058e6905
BenjiReis <benjamin.reis@vates.fr>
2020-11-04 12:29:41 +0100

    test list domain

--- rust/daemon/main.rs
+++ rust/daemon/main.rs
@@ -96,10 +96,6 @@ fn main () {
 	response = r#"{"jsonrpc":"2.0","result":"success","id":1}"#;
   assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
 
-  request = r#"{"jsonrpc": "2.0", "method": "vm.unpause", "params": { "dom_id": 7 }, "id": 1}"#;
-	response = r#"{"jsonrpc":"2.0","result":"success","id":1}"#;
-  assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
-
   request = r#"{"jsonrpc": "2.0", "method": "vm.unpause", "params": { "dom_id": 12 }, "id": 1}"#;
   response = r#"{"jsonrpc":"2.0","error":{"code":0,"message":"-3: No such process (os error 3) ()"},"id":1}"#;
   assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
@@ -109,7 +105,10 @@ fn main () {
   assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
 
   request = r#"{"jsonrpc": "2.0", "method": "vm.shutdown", "params": { "dom_id": 12 }, "id": 1}"#;
-  response = r#"{"jsonrpc":"2.0","error":{"code":0,"message":"-3: No such process (os error 3) ()"},"id":1}"#;
+  response = r#"{"jsonrpc":"2.0","error":{"code":0,"message":"error"},"id":1}"#;
   assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
 
+  request = r#"{"jsonrpc": "2.0", "method": "host.domain-list", "id": 1}"#;
+  response = r#"{"jsonrpc":"2.0","error":{"code":0,"message":"error"},"id":1}"#;
+  assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
 }
